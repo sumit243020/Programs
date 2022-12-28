313596fcf5c3222c934784517f162c63404ea5cb
sumit243020 <121555429+sumit243020@users.noreply.github.com>
2022-12-28 17:50:52 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,25 @@
+package demo;
+
+public class sum {
+	
+	public  void add() {
+		int a=20;
+		int b=30;
+		
+		int add;
+		add=a+b;
+		System.out.println(add);
+		
+		
+	}
+
+	public static void main(String[] args) {
+			
+		sum sobj = new sum();
+		sobj.add();
+		
+		
+
+	}
+
+}
