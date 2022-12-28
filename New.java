9e4b9a6dbc368f6e312bd870121c711a7827e7b3
sumit243020 <121555429+sumit243020@users.noreply.github.com>
2022-12-28 17:50:19 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,21 @@
+package demo;
+import java.util.*;
+public class New {
+	
+	
+	public static void main(String[] args) {
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter First No" );
+		int a = sobj.nextInt();
+		System.out.println("Enter Second No");
+		int b = sobj.nextInt();
+		
+		
+		int sum=a+b;
+		
+		
+		System.out.println("Sum Of two numbers are:"+sum);
+		
+	}
+
+}
